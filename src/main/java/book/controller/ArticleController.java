c1ae5fd0d33eee3ee5300869d27e181238cde68a
sere4580@naver.com <sere4580@naver.com>
2024-02-07 10:08:34 +0900

    SPRING

---
+++ src/main/java/book/controller/ArticleController.java
@@ -0,0 +1,18 @@
+package book.controller;
+
+public class ArticleController {
+    // @Override
+}
+/*
+    @ 어노테이션
+    1. 표준 어노테이션 : 자바에서 기본적으로 제공
+        @Override : 메소드 재정의
+    2. 메타 어노테이션 : P.64
+        소스코드에 추가해서 사용하는 메타 데이터
+        메타 데이터 : 구조화된 데이터
+        컴파일 또는 실행 했을때 코드를 어떻게 처리 해야 할지 알려주는 정보
+        @SpringBootApplication
+        @Controller
+        @GetMapping
+
+*/
