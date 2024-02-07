fb241e062df9e06f7fb44a092168440f003e31a5
sere4580@naver.com <sere4580@naver.com>
2024-02-07 11:21:36 +0900

    SPRING

--- src/main/java/book/controller/ArticleController.java
+++ src/main/java/book/controller/ArticleController.java
@@ -1,7 +1,27 @@
 package book.controller;
 
+import book.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ArticleController {
-    // @Override
+
+    // 1. 글쓰기 페이지 반환
+    @GetMapping("/articles/new")
+    public String newArticleForm(){
+        return "articles/new";  // 확장자 빼고 resources/templates
+    }
+
+    // 2. 글쓰기 처리
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+        System.out.println("ArticleController.createArticle");
+
+        System.out.println("form = " + form);
+        return "";
+    }
 }
 /*
     @ 어노테이션
@@ -12,6 +32,12 @@ public class ArticleController {
         메타 데이터 : 구조화된 데이터
         컴파일 또는 실행 했을때 코드를 어떻게 처리 해야 할지 알려주는 정보
         @SpringBootApplication
+            1. 내장 서버(톰캣) 지원
+            2. 스프링 MVC 패턴 내장
+                view : resources
+                CONTROLLER : @Controller, @RestController
+                SERVICE : @Service
+                MODEL(DAO) : @Repository
         @Controller
         @GetMapping
 
